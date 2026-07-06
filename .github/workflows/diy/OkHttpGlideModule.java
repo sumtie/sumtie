ebfc99497cc600d9971dcbcf270292bde1e9502f
sumtie <154700677+sumtie@users.noreply.github.com>
2026-07-06 20:56:43 +0800

    test.yml

---
+++ .github/workflows/diy/OkHttpGlideModule.java
@@ -0,0 +1,39 @@
+package com.fongmi.android.tv.utils;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+import com.github.catvod.net.OkHttp;
+
+import java.io.InputStream;
+
+/**
+  适用于 fongmi tv v1.8.2 ~ v5.0.0
+*/
+@GlideModule
+public class OkHttpGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder.setLogLevel(Log.ERROR);
+        // 设置磁盘缓存大小为 100MB，推荐100~250MB，不设置默认是250MB
+        int diskCacheSizeBytes = 1024 * 1024 * 100;
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
+    }
+
+    @Override
+    public void registerComponents(@NonNull Context context, @Nullable Glide glide, Registry registry) {
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(OkHttp.client()));
+    }
+}
